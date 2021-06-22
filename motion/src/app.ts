618cd48164f77d0adf23c57604de55b89426ea60
sohee <ksohee1207@gmail.com>
2021-06-22 18:16:59 +0900

    note, todo component

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,4 +1,6 @@
 import { ImageComponent } from './components/page/item/image.js';
+import { NoteComponent } from './components/page/item/note.js';
+import { TodoComponent } from './components/page/item/todo.js';
 import {PageComponent} from './components/page/page.js'
 
 class App{
@@ -9,7 +11,13 @@ class App{
         this.page.attachTo(appRoot);
 
         const image = new ImageComponent('Image Title?', 'https://picsum.photos/600/300');
-        image.attachTo(appRoot, 'beforeend')
+        image.attachTo(appRoot, 'beforeend');
+
+        const note = new NoteComponent("Today's Diary", '잘 지내는 지 걱정 된다. 생각하지 않으려는 순간이 내겐 더 고통스러워.');
+        note.attachTo(appRoot, 'beforeend');
+
+        const todo = new TodoComponent('Todo List', '잠 잘 자기');
+        todo.attachTo(appRoot, 'beforeend');
     }
 }
 
--- motion/src/components/page/item/image.ts
+++ motion/src/components/page/item/image.ts
@@ -7,7 +7,7 @@ export class ImageComponent extends BaseComponent<HTMLElement>{
       super(`<section class="image">
               <div class="image__holder">
                 <img class="image__thumbnail">
-                <p class="image__title"></p>
+                <h2 class="image__title"></h2>
               </div>
             </section>`);
       
@@ -16,7 +16,7 @@ export class ImageComponent extends BaseComponent<HTMLElement>{
       imageElement.src = url;
       imageElement.alt = title;
 
-      const titleElement = this.element.querySelector('.image__title')! as HTMLParagraphElement;
+      const titleElement = this.element.querySelector('.image__title')! as HTMLHeadingElement;;
       titleElement.textContent = title;
     }
 }
